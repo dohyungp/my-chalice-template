e3e0e008e274648e368e8c6a51e12ea8868e951d
Dohyung Park <58218187+dohyungp@users.noreply.github.com>
2021-08-09 23:14:07 +0900

    Update config.py

--- chalicelib/core/config.py
+++ chalicelib/core/config.py
@@ -2,12 +2,10 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    """HolyCafe Configurations"""
+    """Template Configurations"""
 
-    APP_NAME: str = "holycafe"
+    APP_NAME: str = "template"
     STAGE: str = "dev"
-    MEDITHERAPHY_CAFE24_WEBHOOK_KEY: str = "this-is-not-real-key"
-    MEDITHERAPHY_MP_TOKEN: str = "this-is-not-real-key"
 
     class Config:
         """Pydantic Config"""
